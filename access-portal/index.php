c9be74d9908d7e670b711282f31df124521f2669
Issa Rice <riceissa@gmail.com>
2017-09-18 12:48:44 -0700

    initial commit

---
+++ access-portal/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <title>Beliefs repo</title>
+</head>
+
+<body>
+
+<p>
+    Some ways I would describe this are “tracking belief changes over time”,
+    “recording beliefs with high fidelity”, “creating a repository of beliefs”.
+</p>
+
+<p>Some related reading:</p>
+<ul>
+    <li><a href="http://lesswrong.com/lw/klg/knightian_uncertainty_in_a_bayesian_framework/">“Knightian uncertainty in a Bayesian framework”</a></li>
+    <li><a href="http://lesswrong.com/lw/jlh/knightian_uncertainty_from_a_bayesian_perspective/">“Knightian Uncertainty from a Bayesian perspective”</a></li>
+</ul>
+
+<p>Some related things that I know exist:
+</p>
+
+<ul>
+    <li>The <a href=https://en.wikipedia.org/wiki/Initiative_on_Global_Markets#IGM_polls_and_the_Economics_Experts_Panel">IGM Panel</a></li>
+    <li>Propositions on Arbital</li>
+    <li>PhilPapers Surveys and other professional surveys (AI Impacts survey,
+        various surveys of economists)</li>
+    <li>YourMorals.Org</li>
+    <li>OkCupid profile questions</li>
+    <li>Prediction sites like PredictionBook</li>
+    <li>Cost-effectiveness spreadsheets like those by GiveWell</li>
+    <li>Julia Galef’s Update Project</li>
+    <li>Some individuals who have published belief summaries, e.g.
+        <a href="http://reducing-suffering.org/summary-beliefs-values-big-questions/">Brian Tomasik</a></li>
+</ul>
+
+</body>
+</html>
