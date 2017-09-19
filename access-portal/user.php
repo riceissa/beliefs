c571f219e90ff7886f4f4ec76582de87bb78ffd0
Issa Rice <riceissa@gmail.com>
2017-09-19 15:40:29 -0700

    add more columns

--- access-portal/user.php
+++ access-portal/user.php
@@ -19,6 +19,14 @@
       <th>Belief</th>
       <th>Likert response</th>
       <th>Confidence</th>
+      <th>Probability</th>
+      <th>Probability lower bound</th>
+      <th>Probability upper bound</th>
+      <th>Belief held on</th>
+      <th>Belief expressed on</th>
+      <th>Belief entered on</th>
+      <th>Works consumed</th>
+      <th>Notes</th>
     </tr>
   </thead>
   <tbody>
@@ -38,8 +46,16 @@ if ($stmt = $mysqli->prepare($query)) {
 
 <tr>
     <td><a href="/belief.php?text=<?= urlencode($row['belief_text']) ?>"><?= $row['belief_text'] ?? 'N/A' ?></a></td>
-    <td><?= $row['likert_response'] ?></td>
-    <td><?= $row['confidence'] ?></td>
+    <td><?= $row['likert_response'] ?? '&ndash;' ?></td>
+    <td align="right"><?= $row['confidence'] ?? '&ndash;' ?></td>
+    <td align="right"><?= $row['probability_point_estimate'] ?? '&ndash;' ?></td>
+    <td align="right"><?= $row['probability_lower_bound'] ?? '&ndash;' ?></td>
+    <td align="right"><?= $row['probability_upper_bound'] ?? '&ndash;' ?></td>
+    <td align="right"><?= $row['belief_date'] ?? '&ndash;' ?></td>
+    <td align="right"><?= $row['belief_expression_date'] ?? '&ndash;' ?></td>
+    <td align="right"><?= $row['belief_entry_date'] ?? '&ndash;' ?></td>
+    <td><?= $row['works_consumed'] ?? '&ndash;' ?></td>
+    <td><?= $row['notes'] ?? '&ndash;' ?></td>
 </tr>
 
 <?php
