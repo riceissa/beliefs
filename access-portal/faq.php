a7b773425d465d8c3294744cb321df88862a85c8
Issa Rice <riceissa@gmail.com>
2017-09-19 11:02:26 -0700

    add FAQ

---
+++ access-portal/faq.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <title>Beliefs repo</title>
+</head>
+
+<body>
+<nav><a href="/index.php">Home</a>,
+    <a href="/faq.php">FAQ</a>,
+    <a href="/belief.php">Beliefs</a>,
+    <a href="/user.php">Users</a>
+</nav>
+
+
+<h1>Frequently asked questions</h1>
+
+<p><strong>Question:</strong>
+    Why make this site?
+</p>
+
+<p><strong>Question:</strong>
+    Why use IndieAuth for authentication?
+</p>
+
+<p><strong>Question:</strong>
+    Can predictions be entered on this site?
+</p>
+
+<p><strong>Question:</strong>
+    Who runs the site?
+</p>
+<p><strong>Answer:</strong>
+    Issa Rice.
+</p>
+
+<p><strong>Question:</strong>
+    Why does the site look so bad?
+</p>
+
+<p><strong>Question:</strong>
+</p>
+
+<p><strong>Question:</strong>
+</p>
+
+<p><strong>Question:</strong>
+</p>
