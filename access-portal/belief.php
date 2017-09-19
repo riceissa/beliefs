8c7b92c948855743d49c3c83c8049d35d852e86c
Issa Rice <riceissa@gmail.com>
2017-09-18 18:39:30 -0700

    add belief.php

---
+++ access-portal/belief.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <title>Beliefs repo</title>
+</head>
+
+<body>
+
+<p>Belief:
+<?= htmlspecialchars($_REQUEST['text']) ?>
+</p>
+
+</body>
+</html>
