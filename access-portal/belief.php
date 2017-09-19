d12374099197b5d96d9f757e0a108e253a7a5e3d
Issa Rice <riceissa@gmail.com>
2017-09-18 18:57:26 -0700

    put down a preliminary table

--- access-portal/belief.php
+++ access-portal/belief.php
@@ -1,3 +1,8 @@
+<?php
+
+include_once("backend/globalVariables/passwordFile.inc");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +16,35 @@
 <p>Belief:
 <?= htmlspecialchars($_REQUEST['text']) ?>
 </p>
+
+<table>
+  <thead>
+    <tr>
+      <th>Username</th>
+      <th>Likert response</th>
+      <th>Confidence</th>
+    </tr>
+  </thead>
+  <tbody>
+
+<?php
+$query = "select * from beliefs where belief_text = ?";
+if ($stmt = $mysqli->prepare($query)) {
+    $stmt->bind_param("s", $_REQUEST['text']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_assoc()) {
+?>
+
+    <td><?= $row['username'] ?? 'N/A' ?></td>
+    <td><?= $row['likert_response'] ?></td>
+    <td><?= $row['confidence'] ?></td>
+
+<?php
+    }
+}
+
+?>
 
 </body>
 </html>
